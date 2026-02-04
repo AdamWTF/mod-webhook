d715d91e072d23f97dc056a87dc7efaabffe405d
Rikus Louw <rikuswlouw@gmail.com>
2026-02-04 21:28:22 +0200

    fix: used correct loader function

--- src/webhook_loader.cpp
+++ src/webhook_loader.cpp
@@ -8,7 +8,7 @@ void AddWebhookPlayerScripts();
 // Add all
 // cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
 // additionally replace all '-' in the module folder name with '_' here
-void Addacore_webhooksScripts()
+void Addmod_webhookScripts()
 {
     AddWebhookServerScripts();
     AddWebhookPlayerScripts();
