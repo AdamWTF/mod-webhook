427ac05826e8536cb958803510f1e95e97c15898
Adam Fox <a.w.fox90@gmail.com>
2026-06-24 09:05:18 +0100

    Update webhook_player.cpp

--- src/webhook_player.cpp
+++ src/webhook_player.cpp
@@ -4,36 +4,110 @@
 #include "Chat.h"
 #include "WebhookMgr.h"
 
-
 // Add player scripts
 class WebhookPlayerScripts : public PlayerScript
 {
 public:
     WebhookPlayerScripts() : PlayerScript("WebhookPlayerScripts") { }
 
-    void OnPlayerUpdate(Player* player, uint32 /*p_time*/) override
+    std::string ToLower(std::string value)
     {
-       // 1. Get player data
-        uint32 guid = player->GetGUID().GetCounter();
-        uint32 zoneId = player->GetZoneId();
-        std::string name = player->GetName();
-        float x = player->GetPositionX();
-        float y = player->GetPositionY();
-        float z = player->GetPositionZ();
+        std::transform(value.begin(), value.end(), value.begin(),
+            [](unsigned char c)
+            {
+                return std::tolower(c);
+            });
 
-        // 2. Build the JSON object string
-        std::stringstream json;
-        json << "{"
-             << "\"event\": \"player_update\", "
-             << "\"guid\": " << guid << ", "
-             << "\"name\": \"" << name << "\", "
-             << "\"position_x\": " << x << ", "
-             << "\"position_y\": " << y << ", "
-             << "\"position_z\": " << z << ", "
-             << "\"zone_id\": " << zoneId
-             << "}";
+        return value;
+    }
 
-       sWebhookMgr->ScheduleMessage(json.str()); // Schedule the message
+    bool StartsWith(std::string const& value, std::string const& prefix)
+    {
+        return value.size() >= prefix.size()
+            && value.compare(0, prefix.size(), prefix) == 0;
+    }
+
+    bool IsPlayerBot(Player* player)
+    {
+        if (!player || !player->GetSession())
+        {
+            return true;
+        }
+
+        uint32 accountId = player->GetSession()->GetAccountId();
+
+        std::string accountName;
+        if (!AccountMgr::GetName(accountId, accountName))
+        {
+            return false;
+        }
+
+        std::string botAccountPrefix = sConfigMgr->GetOption<std::string>(
+            "AiPlayerbot.RandomBotAccountPrefix",
+            "rndbot"
+        );
+
+        return StartsWith(ToLower(accountName), ToLower(botAccountPrefix));
+    }
+
+    void OnPlayerLogin(Player* player) override
+    {
+        if (IsPlayerBot(player))
+        {
+            return;
+        }
+
+        std::stringstream ss;
+        ss << "Player " << player->GetName() << " just signed in.";
+
+        std::string message = ss.str();
+
+        sWebhookMgr->ScheduleMessage(message);
+    }
+
+    void OnPlayerLogout(Player* player) override
+    {
+        if (IsPlayerBot(player))
+        {
+            return;
+        }
+
+        std::stringstream ss;
+        ss << "Player " << player->GetName() << " just signed out.";
+
+        std::string message = ss.str();
+
+        sWebhookMgr->ScheduleMessage(message);
+    }
+
+    void OnPlayerLevelChanged(Player* player, uint8 oldLevel) override
+    {
+        if (IsPlayerBot(player))
+        {
+            return;
+        }
+
+        std::stringstream ss;
+        ss << "Player " << player->GetName() << " just reached level " << player->getLevel() << ".";
+
+        std::string message = ss.str();
+
+        sWebhookMgr->ScheduleMessage(message);
+    }
+
+    void OnPlayerAchievementComplete(Player* player, AchievementEntry const* achievement) override
+    {
+        if (IsPlayerBot(player))
+        {
+            return;
+        }
+
+        std::stringstream ss;
+        ss << "Player " << player->GetName() << " just earned the achievement " << achievement->Title << ".";
+
+        std::string message = ss.str();
+
+        sWebhookMgr->ScheduleMessage(message);
     }
 };
 
